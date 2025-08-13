f1f68746705ceccadd91f6038d156f8e9a381151
Arun Meena <meenaarun738@gmail.com>
2025-08-13 22:48:48 +0530

    Updated responsive code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ function Navbar() {
 
   return (
     <div>
-      <div className="fixed left-0 w-full bg-white font-montserrat flex justify-between shadow-sm border-b p-3 z-50">
-        
+      
+      <div className=" top-0 fixed left-0 w-full bg-white font-montserrat flex justify-between shadow-sm border-b p-3 z-50">
         <div className="flex items-center gap-1">
           <img
             className="rounded-full object-cover w-8 h-8"
@@ -24,6 +24,7 @@ function Navbar() {
           <li><a href="#contact" className="hover:text-blue-500">Contact</a></li>
         </ul>
 
+        
         <button
           onClick={() => setMenuOpen(!menuOpen)}
           className="md:hidden flex flex-col gap-1 pr-4"
@@ -34,18 +35,59 @@ function Navbar() {
         </button>
       </div>
 
-      
-      <ul
-        className={`md:hidden flex flex-col gap-4 bg-white shadow-md p-4 border-t fixed top-14 left-0 w-full z-40 transform transition-transform duration-300 ease-in-out ${
+      <div className={`md:hidden flex flex-col gap-4 bg-white shadow-md p-4 border-t fixed top-14 left-0 w-full z-40 transform transition-transform duration-300 ease-in-out ${
           menuOpen ? "translate-y-0" : "-translate-y-full"
-        }`}
+        }`}>
+      <ul
+        
       >
-        <li><a href="#home" className="hover:text-blue-500">Home</a></li>
-        <li><a href="#about" className="hover:text-blue-500">About</a></li>
-        <li><a href="#project" className="hover:text-blue-500">Projects</a></li>
-        <li><a href="#experience" className="hover:text-blue-500">Experience</a></li>
-        <li><a href="#contact" className="hover:text-blue-500">Contact</a></li>
+        <li>
+          <a
+            href="#home"
+            className="hover:text-blue-500"
+            onClick={() => setMenuOpen(false)}
+          >
+            Home
+          </a>
+        </li>
+        <li>
+          <a
+            href="#about"
+            className="hover:text-blue-500"
+            onClick={() => setMenuOpen(false)}
+          >
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            href="#project"
+            className="hover:text-blue-500"
+            onClick={() => setMenuOpen(false)}
+          >
+            Projects
+          </a>
+        </li>
+        <li>
+          <a
+            href="#experience"
+            className="hover:text-blue-500"
+            onClick={() => setMenuOpen(false)}
+          >
+            Experience
+          </a>
+        </li>
+        <li>
+          <a
+            href="#contact"
+            className="hover:text-blue-500"
+            onClick={() => setMenuOpen(false)}
+          >
+            Contact
+          </a>
+        </li>
       </ul>
+      </div>
     </div>
   );
 }
