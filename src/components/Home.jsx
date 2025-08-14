df9190f334d0c4c57e976f3eca7834844d624e7a
Arun Meena <meenaarun738@gmail.com>
2025-08-14 14:03:54 +0530

    Updated project

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,9 +21,9 @@ function Home() {
         </div>
 
         <div className="flex flex-col md:flex-row gap-10 md:gap-36 pt-12 md:pt-28">
-          {/* Available On */}
-          <div className="font-bold text-xl pl-4 md:pl-16">
-            Available on
+          
+          <div className="font-bold text-xl pl-8 md:pl-16">
+            <div className="pl-4">Available on</div>
             <div className="flex flex-wrap pt-2 gap-3">
               <a
                 href="https://www.linkedin.com/in/arunmeena0312"
@@ -66,10 +66,10 @@ function Home() {
             </div>
           </div>
 
-          {/* Currently Working On */}
-          <div className="font-bold text-xl pl-4 md:pl-6 pt-4 md:pt-12">
+          
+          <div className="font-bold text-xl pl-4 md:pl-6 pt-1">
             Currently working on
-            <div className="flex flex-wrap pt-2 gap-3">
+            <div className="flex flex-wrap pt-2 pl-6  gap-3">
               <img className="cursor-pointer w-8 h-8" src="/mongodb.png" alt="mongodb" />
               <img className="cursor-pointer w-8 h-8" src="/expressjs.svg" alt="express" />
               <img className="cursor-pointer w-8 h-8" src="/reactt.png" alt="react" />
@@ -79,7 +79,7 @@ function Home() {
         </div>
       </div>
 
-      {/* Profile Image */}
+     
       <div className="flex justify-center md:justify-end pt-10 md:pt-20">
         <img
           className="rounded-full w-64 h-64 md:w-[400px] md:h-[375px] object-cover"
